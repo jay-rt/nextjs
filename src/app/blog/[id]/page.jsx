cd5cd1e990fce3172f0f962784ba1d786aa84404
jay-rt <thapa.jayrt@gmail.com>
2023-07-15 00:46:52 -0400

    added main blog page and individual blog page

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,5 +1,51 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
 const BlogPost = () => {
-  return <div>BlogPost</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.top}>
+        <div className={styles.info}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+          <div className={styles.author}>
+            <Image
+              src="https://images.pexels.com/photos/872756/pexels-photo-872756.jpeg?auto=compress&cs=tinysrgb&w=600"
+              alt=""
+              width={40}
+              height={40}
+              className={styles.avatar}
+            />
+            <span className={styles.username}>John Doe</span>
+          </div>
+        </div>
+        <div className={styles.img__container}>
+          <Image
+            src="https://images.pexels.com/photos/9391321/pexels-photo-9391321.jpeg?auto=compress&cs=tinysrgb&w=600"
+            alt=""
+            fill={true}
+            className={styles.img}
+          />
+        </div>
+      </div>
+      <div className={styles.bottom}>
+        <p className={styles.text}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae
+          dolor, optio voluptatibus magnam iure esse tempora beatae. A suscipit
+          eos. Animi quibusdam cum omnis officiis voluptatum quo ea eveniet?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus quae
+          dolor, optio voluptatibus magnam iure esse tempora beatae, a suscipit
+          eos. Animi quibusdam cum omnis officiis
+          <br />
+          <br />
+          voluptatum quo ea eveniet? Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Ducimus quae dolor, optio voluptatibus magnam iure
+          esse tempora beatae, a suscipit eos. Animi quibusdam cum omnis
+          officiis voluptatum quo ea eveniet?
+        </p>
+      </div>
+    </div>
+  );
 };
 
 export default BlogPost;
--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,5 +1,57 @@
+import styles from "./page.module.css";
+import Link from "next/link";
+import Image from "next/image";
+
 const Blog = () => {
-  return <div>Blog</div>;
+  return (
+    <div className={styles.main__container}>
+      <Link href="/blog/blogId" className={styles.container}>
+        <div className={styles.image__container}>
+          <Image
+            src="https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.img}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blogId" className={styles.container}>
+        <div className={styles.image__container}>
+          <Image
+            src="https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.img}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+      <Link href="/blogId" className={styles.container}>
+        <div className={styles.image__container}>
+          <Image
+            src="https://images.pexels.com/photos/624015/pexels-photo-624015.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            alt=""
+            width={400}
+            height={250}
+            className={styles.img}
+          />
+        </div>
+        <div className={styles.content}>
+          <h1 className={styles.title}>Test</h1>
+          <p className={styles.desc}>Desc</p>
+        </div>
+      </Link>
+    </div>
+  );
 };
 
 export default Blog;
